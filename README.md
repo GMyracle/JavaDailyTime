b2acf12da06e2678abdb4cb56a222b165a88d433
GMyracle <95431165+GMyracle@users.noreply.github.com>
2023-08-08 18:26:45 +0700

    add to feature-controllerAdvice

--- README.md
+++ README.md
@@ -1,3 +1,4 @@
 # JavaDailyTime
 - 24/07/2023: Update change logical of controller
 - 25/07/2023: Update change response handler
+- 28/07/2023: Update folder finish ResponseHandler
---
+++ bookApp/src/main/java/com/app/bookApp/controller/ControllerAdvisor.java
@@ -0,0 +1,61 @@
+package com.app.bookApp.controller;
+
+import com.app.bookApp.exception.BookNotFoundException;
+import net.minidev.json.JSONObject;
+import org.springframework.context.support.DefaultMessageSourceResolvable;
+import org.springframework.http.HttpHeaders;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.MethodArgumentNotValidException;
+import org.springframework.web.bind.annotation.ControllerAdvice;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.context.request.WebRequest;
+import org.springframework.web.servlet.mvc.method.annotation.ResponseEntityExceptionHandler;
+
+import java.time.LocalDate;
+import java.time.LocalDateTime;
+import java.util.LinkedHashMap;
+import java.util.List;
+import java.util.Map;
+import java.util.stream.Collectors;
+
+@ControllerAdvice
+public class ControllerAdvisor extends ResponseEntityExceptionHandler {
+
+    @Override
+    public ResponseEntity<Object> handleMethodArgumentNotValid(
+            MethodArgumentNotValidException ex, HttpHeaders headers,
+            HttpStatus status, WebRequest request) {
+
+        Map<String, Object> body = new LinkedHashMap<>();
+        body.put("timestamp", LocalDate.now());
+        body.put("status", status.value());
+        body.put("log", LocalDate.now());
+
+        List<String> errors = ex.getBindingResult()
+                .getFieldErrors()
+                .stream()
+                .map(DefaultMessageSourceResolvable::getDefaultMessage)
+                .collect(Collectors.toList());
+
+        body.put("errors", errors);
+        JSONObject json = new JSONObject(body);
+
+        headers.add(HttpHeaders.CONTENT_TYPE, "application/json"); // Thêm tiêu đề kiểu dữ liệu
+        return new ResponseEntity<>(json, headers, HttpStatus.BAD_REQUEST);
+    }
+
+    @ExceptionHandler(BookNotFoundException.class)
+    public ResponseEntity<Object> handleNodataFoundException(
+            BookNotFoundException ex, WebRequest request) {
+
+        Map<String, Object> body = new LinkedHashMap<>();
+        body.put("timestamp", LocalDateTime.now());
+        body.put("message", "No cities found");
+
+        HttpHeaders headers = new HttpHeaders();
+        headers.add(HttpHeaders.CONTENT_TYPE, "application/json"); // Thêm tiêu đề kiểu dữ liệu
+
+        return new ResponseEntity<>(body,headers, HttpStatus.NOT_FOUND);
+    }
+}
